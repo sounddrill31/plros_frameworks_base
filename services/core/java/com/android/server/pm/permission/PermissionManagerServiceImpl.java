85b3f674f1a5768e8675660ca8c613708f258343
Daniel Micay <danielmicay@gmail.com>
2017-10-07 22:54:42 +0300

    add special runtime permission for other sensors
    Ported from 12: 9d5a62ed573bc3c7be8b19445b372fed13533d0e

--- services/core/java/com/android/server/pm/permission/PermissionManagerServiceImpl.java
+++ services/core/java/com/android/server/pm/permission/PermissionManagerServiceImpl.java
@@ -2524,7 +2524,7 @@ public class PermissionManagerServiceImpl implements PermissionManagerServiceInt
     }
 
     public static boolean isSpecialRuntimePermission(final String permission) {
-        return false;
+        return Manifest.permission.OTHER_SENSORS.equals(permission);
     }
 
     /**
