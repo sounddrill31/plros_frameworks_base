ddab132b86e4cf9c30ea844ddf144284a2e446de
SagarMakhar <sagarmakhar@gmail.com>
2021-11-06 14:09:41 +0000

    Make "Require unlocking to use sensitive QS tiles" optional [1/2]
    Change-Id: I355ef643fc046ce50f40924db130ad212fea5800
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5570,6 +5570,11 @@ public final class Settings {
         public static final String GAMESPACE_SUPPRESS_FULLSCREEN_INTENT = "gamespace_suppress_fullscreen_intent";
 
         /**
+         * @hide
+         */
+        public static final String USE_TILES_ON_SECURE_KEYGUARD = "use_tiles_on_secure_keyguard";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
