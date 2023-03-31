4daff7d7dc0572eaee15fa7fe707ff8c76d63bf5
Ido Ben-Hur <idoybh2@gmail.com>
2023-03-31 04:02:24 +0300

    SystemUI: Skip screenrecord dialog when long pressing the QS tile
    Assuming the dialog was ever shown before.

--- packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordDialog.java
+++ packages/SystemUI/src/com/android/systemui/screenrecord/ScreenRecordDialog.java
@@ -69,6 +69,7 @@ public class ScreenRecordDialog extends SystemUIDialog {
     private static final long INTERVAL_MS = 1000;
     private static final String TAG = "ScreenRecordDialog";
     private static final String PREFS = "screenrecord_";
+    private static final String PREF_SHOWN = "shown";
     private static final String PREF_TAPS = "show_taps";
     private static final String PREF_LOW = "use_low_quality";
     private static final String PREF_HEVC = "use_hevc";
@@ -225,5 +226,36 @@ public class ScreenRecordDialog extends SystemUIDialog {
                 requestScreenCapture(captureTarget);
             }
         }
+    }
+
+    public static boolean wasShown(Context context) {
+        // if shown pref is set to 1, or anything else is diff than the default (for migration)
+        return (Prefs.getInt(context, PREFS + PREF_SHOWN, 0) != 0 ||
+                Prefs.getInt(context, PREFS + PREF_TAPS, 0) != 0 ||
+                Prefs.getInt(context, PREFS + PREF_LOW, 0) != 0 ||
+                Prefs.getInt(context, PREFS + PREF_HEVC, 1) != 1 ||
+                Prefs.getInt(context, PREFS + PREF_AUDIO, 0) != 0 ||
+                Prefs.getInt(context, PREFS + PREF_AUDIO_SOURCE, 0) != 0);
+    }
+
+    public static void startByPrefs(Context context, RecordingController controller) {
+        boolean showTaps = Prefs.getInt(context, PREFS + PREF_TAPS, 0) == 1;
+        boolean lowQuality = Prefs.getInt(context, PREFS + PREF_LOW, 0) == 1;
+        boolean hevc = Prefs.getInt(context, PREFS + PREF_HEVC, 1) == 1;
+        ScreenRecordingAudioSource audioMode = Prefs.getInt(context, PREFS + PREF_AUDIO, 0) == 1
+                ? (ScreenRecordingAudioSource) MODES.get(Prefs.getInt(context, PREFS + PREF_AUDIO_SOURCE, 0))
+                : NONE;
+        PendingIntent startIntent = PendingIntent.getForegroundService(context,
+                RecordingService.REQUEST_CODE,
+                RecordingService.getStartIntent(
+                        context, Activity.RESULT_OK,
+                        audioMode.ordinal(), showTaps, null,
+                        lowQuality, hevc),
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        PendingIntent stopIntent = PendingIntent.getService(context,
+                RecordingService.REQUEST_CODE,
+                RecordingService.getStopIntent(context),
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        controller.startCountdown(DELAY_MS, INTERVAL_MS, startIntent, stopIntent);
     }
 }
